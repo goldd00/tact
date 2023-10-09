ce9f3f5443ebf2cdea314994262d711bfb401aaa
stakasawa <stakasawa@stakasawanoMac-mini.local>
2023-10-09 12:59:41 +0900

    contact不具合修正

--- parts/button/contact.php
+++ parts/button/contact.php
@@ -1,5 +1,5 @@
-<div class="ContactBtn js_moreBtn js_info" data-type="mail">
-    <a href="mailto:yyamamoto@tact-japan.com?subject=&lt;お問い合わせ内容とご連絡先をお送りください&gt;" class="ContactBtn__link">
+<div class="ContactBtn js_moreBtn">
+    <a href="mailto:yyamamoto@tact-japan.com?subject=&lt;お問い合わせ内容とご連絡先をお送りください&gt;" class="ContactBtn__link js_info"  data-type="mail">
         <span class="ContactBtn__link__text">CONTACT</span>
         <span class="ContactBtn__link__icon"><img src="<?php echo imdir(); ?>/common/icon_mail.svg" class="ContactBtn__link__icon__img" alt=""></span>
     </a>
